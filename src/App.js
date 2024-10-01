1f322dd67d8aae9ba6149d8aa174ea75111d8864
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-10-01 16:06:18 +0900

    useEffect 사용시, destroy 될때 불리는 예제 작성

--- src/App.js
+++ src/App.js
@@ -2,15 +2,48 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created ->");
+    return () => console.log("<- destroyed");
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onChangeShowing = () => setShowing(prev => !prev);
+
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter(prev => prev + 1);
 
-  const runOnlyOnce = () => console.log("I run only once time");
-  useEffect(runOnlyOnce, []);
+  const [keyword, setKeyword] = useState("");
+  const onSearch = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR :", keyword);
+    }
+  }, [keyword]);
 
   return (
     <div>
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onChangeShowing}>{showing ? "Hide" : "Show"}</button>
+      </div>
+
+      <hr />
+
+      <input type="text" placeholder="search here"
+        value={keyword}
+        onChange={onSearch}
+      />
       <h1 className={styles.title}>{counter}</h1>
       <Button text={"click me"} onClick={onClick} />
     </div>
