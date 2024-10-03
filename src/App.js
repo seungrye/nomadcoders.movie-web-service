25868cd50395ee2143935db3e8dd02b06fac99e8
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-10-03 14:38:58 +0900

    prepare new app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
 import { useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const onChange = (event) => {
-    setTodo(event.target.value);
-  };
-
-  const [todos, setTodos] = useState([]);
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") return;
-
-    setTodos(prev => [...prev, todo]);
-    setTodo("");
-  };
 
   return (
     <div>
-      <h1>My Todo ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text"
-          value={todo}
-          onChange={onChange}
-          placeholder="Write your todo..."
-        />
-        <button>add todo</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
     </div>
   );
 }
